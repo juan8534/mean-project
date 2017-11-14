fb61eb42cd5cebf70c842d01429fe226de9685c3
juan pablo vargas <juanpablo8534@gmail.com>
2017-11-13 21:36:01 -0500

    Se sube el proyecto

---
+++ public/resources/js/app.js
@@ -0,0 +1,342 @@
+
+var app = angular.module('multas', []);
+
+app.controller('multasController', function ($scope, $http) {
+    
+    $scope.loader = {
+        loading: false
+    };
+    
+    $scope.showCreateForm = function () {
+        // clear form
+        $scope.clearForm();
+
+        // change modal title
+        $('#modal-product-title').text('Añadir calificaciones');
+
+        // hide update product button
+        $('#btn-update-product').hide();
+
+        // show create product button
+        $('#btn-create-product').show();
+
+    };
+
+    // Clear form values
+    $scope.clearForm = function () {
+        $scope.id = "";
+        $scope.nocontrol = "";
+        $scope.materia = "";
+        $scope.calificacion = "";
+    };
+    
+    // Hide Form fields 
+    $scope.hideFormFields = function () {
+        $('#form-dinminder').hide();
+    };
+    
+    // Show Form fields 
+    $scope.showFormFields = function () {
+        $('#form-dinminder').show();
+    };
+
+    //Read all entries
+    $scope.getAll = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.get("api/list")
+            .success(function (response) {
+                if (response.error === 2) {
+					//if error code is returned from node code, then there are no entries in db!
+					$scope.statustext = "There are currently no products available!";
+					$scope.loader.loading = false;
+				} else {
+					$scope.datos = response.multa;
+					//Turn off spinner
+					$scope.loader.loading = false;
+					$scope.statustext = "";
+				}
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.statustext = "There was an error fetching data, please check database connection!";
+            });
+    };
+
+    // Read product by ID
+    $scope.readOne = function (id) {
+        // clear modal content
+        $scope.clearForm();
+        $scope.hideFormFields();
+        
+        // change modal title
+        $('#modal-product-title').text("Editar Calificacion");
+
+        // show udpate product button
+        $('#btn-update-product').show();
+
+        // show create product button
+        $('#btn-create-product').hide();
+        
+        $scope.loader.loading = true;
+
+        // get id 
+        $http.get('api/list/' + id)
+            .success(function (data, status, headers, config) {
+                //Show fields
+                $scope.showFormFields();
+                // put the values in form
+                $scope.id = data.calificaciones[0].id;
+                $scope.nocontrol_alumno = data.calificaciones[0].nocontrol_alumno;
+                $scope.materia = data.calificaciones[0].materia;
+                $scope.calificacion = data.calificaciones[0].calificacion;
+
+                // show modal
+                $('#myModal').modal('show');
+                //Turn off spinner
+                $scope.loader.loading = false;
+            })
+            .error(function (data, status, headers, config) {
+                //Turn of spinner & display error
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "There was an error fetching data";
+            });
+    };
+
+    // Create Product
+    $scope.createCalificacion = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.post('/api/insert', {
+            'nocontrol_alumno' : $scope.nocontrol_alumno,
+            'materia' : $scope.materia,
+            'calificacion' : $scope.calificacion
+        })
+            .success(function (data, status, headers, config) {
+                // close modal
+                $('#myModal').modal('hide');
+
+                // clear modal content
+                $scope.clearForm();
+
+                // refresh the product list
+                $scope.getAll();
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "No se pudieron insertar los datos!!!";
+            });
+    };
+	
+	// update product record / save changes
+    $scope.updateCalificacion = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.put('/api/update', {
+            'id' : $scope.id,
+            'nocontrol_alumno' : $scope.nocontrol_alumno,
+            'materia' : $scope.materia,
+            'calificacion' : $scope.calificacion
+        })
+            .success(function (data, status, headers, config) {
+                // close modal
+                $('#myModal').modal('hide');
+
+                // clear modal content
+                $scope.clearForm();
+
+                // refresh the product list
+                $scope.getAll();
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "Unable to Update data!";
+            });
+    };
+
+    //Delete product
+    $scope.deleteCalificacion = function (id) {
+        $scope.loader.loading = true;
+		
+        $http.post('/api/delete', {
+            'id' : id
+        })
+            .success(function (data, status, headers, config) {
+				//The modal id is the #confirm + id ( d.id ) passed into function.
+				//The confirm modal is unique to the ID (#confirm+ID) of the product.
+                $('#confirm' + id).modal('hide');
+                // refresh the list
+                $scope.getAll();
+            })
+            .error(function (data, status, headers, config) {
+                $scope.modalstatustext = "Unable to delete data!";
+				// refresh the list
+                $scope.getAll();
+            });
+    };
+
+
+
+});
+/**
+ * Users Controller
+ */
+var app = angular.module('users', []);
+
+app.controller('usersController', function ($scope, $http) {
+    
+    $scope.loader = {
+        loading: false
+    };
+
+    $scope.clearForm = function () {
+        $scope.id_cedulapro = "";
+        $scope.nombre = "";
+        $scope.apellido = "";        
+    };
+
+    $scope.hideFormFields = function () {
+        $('#form-dinminder').hide();
+    };
+
+    /**
+    * Functions angular Users
+    */
+
+    $scope.getAllUsers = function () {
+        
+        $scope.loader.loading = true;
+    
+    //List Users
+    $http.get('api/list/user')
+        .success(function (response) {
+            if (response.error === 2) {
+			    //if error code is returned from node code, then there are no entries in db!
+				$scope.statustext = "There are currently no products available!";
+				$scope.loader.loading = false;
+			} else {
+				$scope.datos = response.user;
+				//Turn off spinner
+				$scope.loader.loading = false;
+				$scope.statustext = "";
+				}
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.statustext = "There was an error fetching data, please check database connection!";
+            });
+    };
+
+
+    //Create USers
+    $scope.createUser = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.post('/api/insert/user', {
+            'id_cedulapro' : $scope.id_cedulapro,
+            'nombre' : $scope.nombre,
+            'apellido' : $scope.apellido
+        })
+            .success(function (data, status, headers, config) {                
+                window.location.href = '/index.html';
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "No se pudieron insertar los datos!!!";
+            });
+    };
+
+    //Delete Users
+    $scope.deleteUser = function (id) {
+        $scope.loader.loading = true;
+		
+        $http.post('/api/delete/user', {
+            'id_cedulapro' : id
+        })
+            .success(function (data, status, headers, config) {
+				//The modal id is the #confirm + id ( d.id ) passed into function.
+				//The confirm modal is unique to the ID (#confirm+ID) of the product.
+                $('#confirm' + id).modal('hide');
+                // refresh the list
+                $scope.getAllUsers();
+            })
+            .error(function (data, status, headers, config) {
+                $scope.modalstatustext = "Unable to delete data!";
+				// refresh the list
+                $scope.getAllUsers();
+            });
+    };
+
+    //Read One
+    $scope.readOneUser = function (id) {
+        
+        // clear modal content
+        $scope.clearForm();
+       // $scope.hideFormFields();
+        
+        // change modal title
+        $('#modal-product-title').text("Editar Usuario");
+
+        // show udpate product button
+        $('#btn-update-product').show();
+
+        // show create product button
+        $('#btn-create-product').hide();
+        
+        $scope.loader.loading = true;
+
+        // get id 
+        $http.get('api/list/user' + id)
+            .success(function (data, status, headers, config) {                
+                // put the values in form
+                $scope.id_cedulapro = data.user[0].id_cedulapro;
+                $scope.nombre = data.user[0].nombre;
+                $scope.apellido = data.user[0].apellido;                
+
+                // show modal
+                $('#myModal').modal('show');
+                //Turn off spinner
+                $scope.loader.loading = false;
+            })
+            .error(function (data, status, headers, config) {
+                //Turn of spinner & display error
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "There was an error fetching data";
+            });
+    };
+
+    // update product record / save changes
+    $scope.updateUser = function () {
+        
+        $scope.loader.loading = true;
+        
+        $http.put('/api/update/user', {
+            'id_cedulapro' : $scope.id_cedulapro,
+            'nombre' : $scope.nombre,
+            'apellido' : $scope.apellido            
+        })
+            .success(function (data, status, headers, config) {
+                window.location.href = '/usuarios.html';                
+            })
+            .error(function (data, status, headers, config) {
+                $scope.loader.loading = false;
+                $scope.modalstatustext = "Unable to Update data!";
+            });
+    };
+
+});
+
+/**
+ * Vehicles Contorller
+ */
+
+var app = angular.module('vehicles', []);
+
+app.controller('vehiclesController', function ($scope, $http) {
+
+
+});
---
+++ server.js
@@ -0,0 +1,385 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+
+
+//LOGGER
+var log4js = require('log4js');
+//log4js.configure('../config/log4js.json');
+var log = log4js.getLogger("server");
+
+
+//STATIC FILES
+app.use(express.static('public'));
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Body parser use JSON data
+
+/*MY SQL Connection Info*/
+var pool = mysql.createPool({
+	connectionLimit : 25,
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'multas',
+	port :'3306'
+});
+
+log.debug('Server is starting....');
+
+//TEST CONNECTION
+pool.getConnection(function (err, connection) {
+	if (!err) {
+		console.log("Conexion a la base de datos exitosa!!! ... ");
+		log.info('La base de datos esta conectada!!! ... ');
+		connection.release();
+	} else {
+		console.log("Error conectando a la base de datos ... ");
+		log.error('Error connectando a la base de datos ... ');
+	}
+	console.log("Realizando conexion ... ");
+});
+
+// ROOT - Loads Angular App
+app.get('/', function (req, res) {
+	res.sendFile( __dirname + "/" + "index.html" );
+});
+/**
+ * Functions Sanction
+ */
+// This responds a GET request for the /list page.
+app.get('/api/list', function (req, res) {
+	console.log("GET Request :: /list");
+	log.info('GET Request :: /list');
+	var data = {
+        "error": 1,
+        "multa": ""
+    };
+	
+	pool.getConnection(function (err, connection) {
+		connection.query('SELECT nombre,apellido,fecha_multa,id_placa,estado_multa,valor_multa from multa, propietario WHERE propietario.id_cedulapro = multa.id_cedula;', function (err, rows, fields) {
+			connection.release();
+
+			if (rows.length !== 0 && !err) {
+				data["error"] = 0;
+				data["multa"] = rows;
+				res.json(data);
+			} else if (rows.length === 0) {
+				//Error code 2 = no rows in db.
+				data["error"] = 2;
+				data["multa"] = 'No hay calificaciones encontradas..';
+				res.json(data);
+			} else {
+				data["multa"] = 'Error al realizar la consulta';
+				res.json(data);
+				console.log('Error al realizar la consulta: ' + err);
+				log.error('Error al realizar la consulta: ' + err);
+			}
+		});
+	
+	});
+});
+
+//UPDATE sanction
+app.put('/api/update', function (req, res) {
+    var id = req.body.id;
+    var nocontrol_alumno = req.body.nocontrol_alumno;
+    var materia = req.body.materia;
+    var calificacion = req.body.calificacion;
+    var data = {
+        "error": 1,
+        "calificaciones": ""
+    };
+	console.log('PUT Request :: /update: ' + id);
+	log.info('PUT Request :: /update: ' + id);
+    if (!!id && !!nocontrol_alumno && !!materia && !!calificacion) {
+		pool.getConnection(function (err, connection) {
+			connection.query("UPDATE calificaciones SET nocontrol_alumno = ?, materia = ?, calificacion = ? WHERE id=?",[nocontrol_alumno,  materia, calificacion, id], function (err, rows, fields) {
+				if (!!err) {
+					data["calificaciones"] = "Error actualizando los datos!!";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["error"] = 0;
+					data["calificaciones"] = "Registro actualiado correctamente";
+					console.log("Actualizado: " + [id, nocontrol_alumno, materia, calificacion]);
+					log.info("Actualizado: " + [id, nocontrol_alumno, materia, calificacion]);
+				}
+				res.json(data);
+			});
+		});
+    } else {
+        data["calificaciones"] = "Porfavor llene todos los caampos (i.e : id, nocontrol, materia, calificacion)";
+        res.json(data);
+    }
+});
+
+//LIST sanction by ID
+/* app.get('/api/list/:id', function (req, res) {
+	var id = req.params.id;
+	var data = {
+        "error": 1,
+        "calificaciones": ""
+    };
+	
+	console.log("GET request :: /list/" + id);
+	log.info("GET request :: /list/" + id);
+	pool.getConnection(function (err, connection) {
+		connection.query('SELECT * from calificaciones WHERE id = ?', id, function (err, rows, fields) {
+			connection.release();
+			
+			if (rows.length !== 0 && !err) {
+				data["error"] = 0;
+				data["calificaciones"] = rows;
+				res.json(data);
+			} else {
+				data["calificaciones"] = 'No product Found..';
+				res.json(data);
+				console.log('Error while performing Query: ' + err);
+				log.error('Error while performing Query: ' + err);
+			}
+		});
+	
+	});
+}); */ 
+
+//INSERT new sanction
+app.post('/api/insert', function (req, res) {
+    var nocontrol_alumno = req.body.nocontrol_alumno;
+    var materia = req.body.materia;
+    var calificacion = req.body.calificacion;
+    var data = {
+        "error": 1,
+        "calificaciones": ""
+    };
+	console.log('POST Request :: /insert: ');
+	log.info('POST Request :: /insert: ');
+    if (!!nocontrol_alumno && !!materia && !!calificacion) {
+		pool.getConnection(function (err, connection) {
+			connection.query("INSERT INTO calificaciones SET nocontrol_alumno = ?, materia = ?, calificacion = ?",[nocontrol_alumno,  materia, calificacion], function (err, rows, fields) {
+				if (!!err) {
+					data["calificaciones"] = "Error Adding data";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["error"] = 0;
+					data["calificaciones"] = "Registro añadido correctamente";
+					console.log("Added: " + [nocontrol_alumno, materia, calificacion]);
+					log.info("Added: " + [nocontrol_alumno, materia, calificacion]);
+				}
+				res.json(data);
+			});
+        });
+    } else {
+        data["calificaciones"] = "Todos los campos son obligatorios!!!";
+        res.json(data);
+    }
+});
+
+//DELETE sanction
+app.post('/api/delete', function (req, res) {
+    var id = req.body.id;
+    var data = {
+        "error": 1,
+        "calificaciones": ""
+    };
+	console.log('DELETE Request :: /delete: ' + id);
+	log.info('DELETE Request :: /delete: ' + id);
+    if (!!id) {
+		pool.getConnection(function (err, connection) {
+			connection.query("DELETE FROM calificaciones WHERE id=?",[id],function (err, rows, fields) {
+				if (!!err) {
+					data["calificaciones"] = "Error deleting data";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["calificaciones"] = 0;
+					data["calificaciones"] = "Registro eliminado correctamente";
+					console.log("Deleted: " + id);
+					log.info("Deleted: " + id);
+				}
+				res.json(data);
+			});
+		});
+    } else {
+        data["calificaciones"] = "Please provide all required data (i.e : id ) & must be a integer";
+        res.json(data);
+    }
+});
+
+
+/**
+ * Functions User
+ */
+
+ //Create User
+app.post('/api/insert/user', function (req, res) {
+    var id_cedulapro = req.body.id_cedulapro;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var data = {
+        "error": 1,
+        "propietario": ""
+    };
+	console.log('POST Request :: /insert: ');
+	log.info('POST Request :: /insert: ');
+    if (!!id_cedulapro && !!nombre && !!apellido) {
+		pool.getConnection(function (err, connection) {
+			connection.query("INSERT INTO propietario SET id_cedulapro = ?, nombre = ?, apellido = ?",[id_cedulapro,  nombre, apellido], function (err, rows, fields) {				
+				if (!!err) {
+					data["propieatario"] = "Error insertando registro";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["error"] = 0;
+					data["propieatario"] = "Registro añadido correctamente";
+					console.log("Added: " + [id_cedulapro, nombre, apellido]);
+					log.info("Added: " + [id_cedulapro, nombre, apellido]);					
+				}
+				res.json(data);
+			});
+        });
+    } else {
+        data["propietario"] = "Todos los campos son obligatorios!!!";
+        res.json(data);
+    }
+});
+
+ //List Users
+app.get('/api/list/user', function (req, res) {
+	console.log("GET Request :: /list");
+	log.info('GET Request :: /list');
+	var data = {
+        "error": 1,
+        "user": ""
+    };
+	
+	pool.getConnection(function (err, connection) {
+		connection.query('SELECT id_cedulapro,nombre,apellido from propietario;', function (err, rows, fields) {
+		connection.release();
+
+			if (rows.length !== 0 && !err) {
+				data["error"] = 0;
+				data["user"] = rows;
+				res.json(data);
+			} else if (rows.length === 0) {
+			//Error code 2 = no rows in db.
+				data["error"] = 2;
+				data["user"] = 'No hay usuarios encontrados..';
+				res.json(data);
+			} else {
+				data["user"] = 'Error al realizar la consulta';
+				res.json(data);
+				console.log('Error al realizar la consulta: ' + err);
+				log.error('Error al realizar la consulta: ' + err);
+			}
+		});
+	
+	});
+
+});
+
+//DELETE sanction
+app.post('/api/delete/user', function (req, res) {
+    var id = req.body.id_cedulapro;
+    var data = {
+        "error": 1,
+        "user": ""
+    };
+	console.log('DELETE Request :: /delete: ' + id);
+	log.info('DELETE Request :: /delete: ' + id);
+    if (!!id) {
+		pool.getConnection(function (err, connection) {
+			connection.query("DELETE FROM propietario WHERE id_cedulapro=?",[id],function (err, rows, fields) {
+				if (!!err) {
+					data["propietario"] = "Error deleting data";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["propietario"] = 0;
+					data["propietario"] = "Registro eliminado correctamente";
+					console.log("Deleted: " + id);
+					log.info("Deleted: " + id);
+				}
+				res.json(data);
+			});
+		});
+    } else {
+        data["propietario"] = "Please provide all required data (i.e : id ) & must be a integer";
+        res.json(data);
+    }
+});
+
+//LIST user by ID
+app.get('/api/list/user:id_cedulapro', function (req, res) {
+	var id = req.params.id_cedulapro;
+	var data = {
+        "error": 1,
+        "user": ""
+    };
+	
+	console.log("GET request :: /list/" + id);
+	log.info("GET request :: /list/" + id);
+	pool.getConnection(function (err, connection) {
+		connection.query('SELECT id_cedulapro,nombre,apellido from propietario WHERE id_cedulapro = ?', id, function (err, rows, fields) {
+			connection.release();
+			
+			if (rows.length !== 0 && !err) {
+				data["error"] = 0;
+				data["user"] = rows;
+				res.json(data);
+			} else {
+				data["user"] = 'No user Found..';
+				res.json(data);
+				console.log('Error while performing Query: ' + err);
+				log.error('Error while performing Query: ' + err);
+			}
+		});
+	
+	});
+});
+
+//UPDATE Users
+app.put('/api/update/user', function (req, res) {
+    var id = req.body.id_cedulapro;
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;    
+    var data = {
+        "error": 1,
+        "user": ""
+    };
+	console.log('PUT Request :: /update: ' + id);
+	log.info('PUT Request :: /update: ' + id);
+    if (!!id && !!nombre && !!apellido) {
+		pool.getConnection(function (err, connection) {
+			connection.query("UPDATE propietario SET id_cedulapro = ?, nombre = ?, apellido = ? WHERE id_cedulapro=?",[id,  nombre, apellido,id], function (err, rows, fields) {
+				if (!!err) {
+					data["user"] = "Error actualizando los datos!!";
+					console.log(err);
+					log.error(err);
+				} else {
+					data["error"] = 0;
+					data["user"] = "Registro actualiado correctamente";
+					console.log("Actualizado: " + [id, nombre, apellido]);
+					log.info("Actualizado: " + [id, nombre, apellido]);
+				}
+				res.json(data);
+			});
+		});
+    } else {
+        data["user"] = "Porfavor llene todos los caampos (i.e : id, nombre, apellido)";
+        res.json(data);
+    }
+});
+
+/**
+ * Function port
+ */
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("La aplicacion funciona por el: " + host + ":" + port);
+
+})
